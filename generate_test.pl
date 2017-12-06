21546c2b386cb175ec290f37a5982041b276dddf
Brandon Cercone <bcerco@gmail.com>
2017-12-06 12:21:33 -0500

    Fixed test case generation.

--- generate_test.pl
+++ generate_test.pl
@@ -4,78 +4,65 @@
 use strict;
 use warnings;
 
-my $tmp;
 my @max;
 my @alloc;
 my @avail;
-my @request;
 my ($numP, $numR) = @ARGV;
 #-- Check for params
 if (not defined $numP or not defined $numR) {die "max numP and numR\n"; }
 
-#-- Randomly choose process to be first in sequence
-my $pid = int(rand($numP));
-#-- Generate available resource vector
-generate_avail(\@avail,1,$numR);
-#-- Generate Max Matrix
-generate_max_matrix(\@max,\@avail,$numP,$numR,$pid);
-#-- Generate Alloc Matrix
-generate_alloc_matrix(\@max,\@alloc,\@avail,$numP,$numR,$pid);
-#-- Generate request vector
-#generate_alloc_matrix(\@avail,\@request,1,$numR);
-
 #-- Print
 printf "%d\n%d\n", $numP, $numR;
-#print_matrix(\@request,1,$numR);
-print_matrix(\@avail,1,$numR);
+#-- Generate available resource vector
+generate_avail(\@avail,$numR);
+print_vector(\@avail,$numR);
+#-- Generate Matrices
+generate_matrices(\@avail,\@max,\@alloc,$numP,$numR);
+
 print_matrix(\@max,$numP,$numR);
 print_matrix(\@alloc,$numP,$numR);
 
 sub generate_avail {
-    for (my $p = 0; $p < $_[1]; $p++){
-        for (my $r = 0; $r < $_[2]; $r++){
-            $_[0][$p][$r] = int(rand(100)) + 10;
+        for (my $r = 0; $r < $_[1]; $r++){
+                $_[0][$r] = int(rand(25));
         }
-    }
 }
 #-- Generate max matrix
-sub generate_max_matrix {
-    for (my $p = 0; $p < $_[2]; $p++){
-        if ($p eq $_[4]){
-            for (my $r = 0; $r < $_[3]; $r++){
-                $tmp = int(rand(100)) + 20;
-                $_[0][$p][$r] = ($tmp > $_[1][0][$r]) ? $tmp : $_[1][0][$r];
-                #$_[0][$p][$r] = $_[1][0][$r] + 10 ;
-            }
+sub generate_matrices {
+        my @randarr;
+        for (my $p = 0; $p < $_[3]; $p++){
+                $randarr[$p] = $p;
         }
-        else{
-            for (my $r = 0; $r < $_[3]; $r++){
-                $_[0][$p][$r] = int(rand(100));
-            }
+        shuf(\@randarr);
+        for (my $p = 0; $p < $_[3]; $p++){
+                my $index = $randarr[$p];
+                for (my $r = 0; $r < $_[4]; $r++){
+                        $_[1][$index][$r] = int(rand(10)) + $_[0][$r];
+                        $_[2][$index][$r] = $_[1][$index][$r] - $_[0][$r];
+                        $_[0][$r] += $_[2][$index][$r];
+                }
         }
-    }
 }
-#-- Generate allocation matrix by using the values from need
-#-- as an upperbound on the rand
-sub generate_alloc_matrix {
-    for (my $p = 0; $p < $_[3]; $p++){
-        if ($p eq $_[5]){
-            for (my $r = 0; $r < $_[4]; $r++){
-                $_[1][$p][$r] = $_[0][$p][$r] - $_[2][0][$r];
-            }
+#-- Randomize array
+sub shuf {
+        my $randarr = shift;
+        my $i = @$randarr;
+        while($i--){
+                my $j = int rand ($i+1);
+                @$randarr[$i,$j] = @$randarr[$j,$i];
         }
-        else{
-            for (my $r = 0; $r < $_[4]; $r++){
-                $_[1][$p][$r] = int(rand($_[0][$p][$r]));
-            }
-        }
-    }
 }
 #-- Print comma separated matrix
 sub print_matrix {
-    for (my $p = 0; $p < $_[1]; $p++){
-        for (my $r = 0; $r < $_[2]; $r++){
-            printf ( $r == $_[2] - 1 ? "$_[0][$p][$r]\n" : "$_[0][$p][$r],");
+        for (my $p = 0; $p < $_[1]; $p++){
+                for (my $r = 0; $r < $_[2]; $r++){
+                        printf ( $r == $_[2] - 1 ? "$_[0][$p][$r]\n" : "$_[0][$p][$r],");
+                }
         }
-    }
+}
+#-- Print comma separated vector
+sub print_vector {
+        for (my $p = 0; $p < $_[1]; $p++){
+                printf ( $p == $_[1] - 1 ? "$_[0][$p]\n" : "$_[0][$p],");
+        }
 }
